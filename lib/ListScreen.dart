909ba1186334e5fd2d4d40173f337234792a6caf
wiktor <wiktor.nizio@gmail.com>
2020-09-06 11:49:33 +0200

    Used a constant.

--- lib/ListScreen.dart
+++ lib/ListScreen.dart
@@ -4,16 +4,17 @@ class ListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    var items = List<ListTile>.generate(100, (i) => ListTile(
+    final items = List<ListTile>.generate(100, (i) => ListTile(
       leading: Icon(Icons.map),
       title: Text('Bookmarked Favorite Locations $i'),
     ),);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("List Screen"),
       ),
       body: ListView(
-          children: items
+          children: items,
       ),
     );
   }
