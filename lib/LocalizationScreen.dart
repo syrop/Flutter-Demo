35fcd2ee6e1dcfeafaa4dfaf4d02dc36eb463bd9
wiktor <wiktor.nizio@gmail.com>
2020-09-06 10:01:59 +0200

    Repracet screen title.

--- lib/LocalizationScreen.dart
+++ lib/LocalizationScreen.dart
@@ -5,13 +5,9 @@ class LocalizationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    var items = List<ListTile>.generate(100, (i) => ListTile(
-      leading: Icon(Icons.map),
-      title: Text('Bookmarked Favorite Locations $i'),
-    ),);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Localizations Screen"),
+        title: Text(DemoLocalizations.of(context).title),
       ),
       body: Center(
         child: Text(
